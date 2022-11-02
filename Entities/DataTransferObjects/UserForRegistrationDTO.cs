eca16160d6f9b5e17935e5c03e495d48550e4734
Kosty <kostya_khvalei@outlook.com>
2022-11-03 00:28:31 +0300

    Update validation attributes for UserForRegistrationDTO

--- Entities/DataTransferObjects/UserForRegistrationDTO.cs
+++ Entities/DataTransferObjects/UserForRegistrationDTO.cs
@@ -9,14 +9,24 @@ namespace Entities.DataTransferObjects
 {
 	public class UserForRegistrationDTO
 	{
+		[StringLength(30, MinimumLength = 2, ErrorMessage = "First Name must have min 2 and max 30 characters")]
 		public string FirstName { get; set; }
+
+		[StringLength(30, MinimumLength = 2, ErrorMessage = "Last Name must have min 2 and max 30 characters")]
 		public string LastName { get; set; }
+
 		[Required(ErrorMessage = "Username is required")]
 		public string UserName { get; set; }
+
 		[Required(ErrorMessage = "Password is required")]
 		public string Password { get; set; }
+
+		[EmailAddress]
 		public string Email { get; set; }
+
+		[Phone]
 		public string PhoneNumber { get; set; }
+
 		public ICollection<string> Roles { get; set; }
 	}
 }
